061f6e65b406a38ecaa1bc6cbb57cd51de2cd125
Nerissa Taylor <taylornerissa@hotmail.com>
2019-08-14 19:02:03 -0400

    first try

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import{connect} from'react-redux'
 
 const AddedFeature = props => {
+  handleChanges
+  
+  }
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button onClick{( props.removeFeatures(removeFeaturesText);
+    setRemoveFeatures} className="button">X</button>
       {props.feature.name}
     </li>
   );
-};
+;
 
 export default AddedFeature;
---
+++ src/reducer/reducer.js
@@ -0,0 +1,10 @@
+import {createStore} from redux;
+import React from 'react';
+
+function reducer(){
+    return {
+        title:};
+    }
+    const store =createStore(reducer);
+    
+}
